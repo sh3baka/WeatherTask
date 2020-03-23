a00f1d57a2bacc37450613c67485609699f03459
Dzims Edijs Dreijers <edijs211@yahoo.com>
2020-03-23 11:22:59 +0200

    Add WeatherController unit tests

--- weather-app/src/main/resources/data.sql
+++ weather-app/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO location VALUES (1, 'Riga', 'LV');
-INSERT INTO location VALUES (2, 'Liepaja', 'LV');
+INSERT INTO location VALUES (1, 'LONDON', 'GB');
+INSERT INTO location VALUES (2, 'manchester', 'GB');
 INSERT INTO user VALUES (1003, 'John', 'Doe', 'johnny2', 2);
 INSERT INTO user VALUES (1002, 'Jane', 'Doe', 'jane24', 2);
 INSERT INTO user VALUES (1001, 'Baby', 'Doe', 'boss', 1);
---
+++ weather-app/src/test/java/com/practice/weatherapp/controller/WeatherControllerTest.java
@@ -0,0 +1,74 @@
+package com.practice.weatherapp.controller;
+
+import com.practice.weatherapp.service.ExternalApiService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration
+class WeatherControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Mock
+    private ExternalApiService externalApiService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void getWeatherByCityIsOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/weather/london")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getWeatherByCityDoesNotExist() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/weather/whah")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getWeatherByUserIsOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/weather/user/johnny2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getWeatherByUserIsNotFound() {
+        assertThrows(NestedServletException.class, () -> mockMvc.perform(MockMvcRequestBuilders
+                .get("/weather/user/johnny23")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+        );
+    }
+}
